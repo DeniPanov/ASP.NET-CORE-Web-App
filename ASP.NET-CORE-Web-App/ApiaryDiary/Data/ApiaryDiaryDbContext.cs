d8f4cd3d781d85ce029f723083974bf2d5e347bb
DeniPanov <Inedvonap@gmail.com>
2020-01-21 07:22:14 +0200

    Initial commit

---
+++ ASP.NET-CORE-Web-App/ApiaryDiary/Data/ApiaryDiaryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiaryDiary.Data
+{
+    public class ApiaryDiaryDbContext : IdentityDbContext
+    {
+        public ApiaryDiaryDbContext(DbContextOptions<ApiaryDiaryDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}

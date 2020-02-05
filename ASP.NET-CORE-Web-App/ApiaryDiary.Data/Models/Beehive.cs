d8f937d4059d99dcf6d253d3c5b3c234808e3116
DeniPanov <Inedvonap@gmail.com>
2020-02-05 08:03:38 +0200

    Add Beehive.cs

--- ASP.NET-CORE-Web-App/ApiaryDiary.Data/Models/Beehive.cs
+++ ASP.NET-CORE-Web-App/ApiaryDiary.Data/Models/Beehive.cs
@@ -1,6 +1,24 @@
 ﻿namespace ApiaryDiary.Data.Models
 {
+    using System.Collections.Generic;
+
+    using ApiaryDiary.Data.Models.Enums;
+
     public class Beehive
     {
+        public Beehive()
+        {
+            Reports = new HashSet<Report>();
+        }
+
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+        public SystemType SystemType { get; set; }
+
+        public bool IsHungry { get; set; }
+
+        public ICollection<Report> Reports { get; set; }
     }
 }

a2c485ac1c0a07421f91fb1124d006405b547034
DeniPanov <Inedvonap@gmail.com>
2020-02-05 08:02:17 +0200

    Add SystemType.cs

---
+++ ASP.NET-CORE-Web-App/ApiaryDiary.Data/Models/Enums/SystemType.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiaryDiary.Data.Models.Enums
+{
+    public enum SystemType
+    {
+        Dadant_Blatt = 1,
+        Langstroth_Ruth = 2,
+        Farrar = 3,
+        Roger_Delon = 4,
+        Polystyrene = 5,
+        Modular = 6,
+        Other = 7
+    }
+}

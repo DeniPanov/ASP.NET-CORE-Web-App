b5655480210a61aee30d9eaf13790e30ee6d84a5
DeniPanov <Inedvonap@gmail.com>
2020-02-10 07:59:29 +0200

    Add all controllers in a separate project

---
+++ ASP.NET-CORE-Web-App/ApiaryDiary.Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿namespace ApiaryDiary.Controllers
+{
+    using System.Diagnostics;
+    using Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET-CORE-Web-App/ApiaryDiary.Controllers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApiaryDiary.Controllers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

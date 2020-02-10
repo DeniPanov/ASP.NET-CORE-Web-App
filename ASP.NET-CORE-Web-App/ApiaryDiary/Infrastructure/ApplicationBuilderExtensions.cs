cb4004dbec210466a5ba31d60088f6514144d4c6
DeniPanov <Inedvonap@gmail.com>
2020-02-10 07:15:47 +0200

    Clean the Startup.cs a little bit, by moving part of the logic in ApplicationBuilderExtensions.cs

---
+++ ASP.NET-CORE-Web-App/ApiaryDiary/Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace ApiaryDiary.Infrastructure
+{
+    using Microsoft.AspNetCore.Builder;
+
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseEndPoints(this IApplicationBuilder app)
+        {
+            return app.UseEndpoints(endpoints =>
+                 {
+                     endpoints.MapControllerRoute(
+                         name: "default",
+                         pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                     endpoints.MapRazorPages();
+                 });
+        }
+    }
+}
--- ASP.NET-CORE-Web-App/ApiaryDiary/Startup.cs
+++ ASP.NET-CORE-Web-App/ApiaryDiary/Startup.cs
@@ -1,13 +1,13 @@
 namespace ApiaryDiary
 {
+    using Infrastructure;
+    using ApiaryDiary.Data;
     using Microsoft.AspNetCore.Builder;
-    using Microsoft.Extensions.Hosting;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using ApiaryDiary.Data;
 
     public class Startup
     {
@@ -18,32 +18,23 @@ namespace ApiaryDiary
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApiaryDiaryDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApiaryDiaryDbContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            app.UseExceptionHandling(env);
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -52,13 +43,7 @@ namespace ApiaryDiary
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
+            app.UseEndPoints();
         }
     }
 }

eb0de1429fe096ae4b023f076d205e01209f9a27
DeniPanov <Inedvonap@gmail.com>
2020-02-10 07:58:22 +0200

    Update ApplicationBuilderExtensions.cs with UseExceptionHandling()

--- ASP.NET-CORE-Web-App/ApiaryDiary/Infrastructure/ApplicationBuilderExtensions.cs
+++ ASP.NET-CORE-Web-App/ApiaryDiary/Infrastructure/ApplicationBuilderExtensions.cs
@@ -1,9 +1,30 @@
 ﻿namespace ApiaryDiary.Infrastructure
 {
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
 
     public static class ApplicationBuilderExtensions
     {
+        public static IApplicationBuilder UseExceptionHandling(
+            this IApplicationBuilder app,
+            IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            return app;
+        }
+
         public static IApplicationBuilder UseEndPoints(this IApplicationBuilder app)
         {
             return app.UseEndpoints(endpoints =>

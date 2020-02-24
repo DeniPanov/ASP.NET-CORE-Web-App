7c7ba1a840de02d5ef6734a100657da0fd447a59
DeniPanov <Inedvonap@gmail.com>
2020-02-24 08:04:42 +0200

    Add QueenBee.cs

--- ASP.NET-CORE-Web-App/ApiaryDiary.Data/Models/QueenBee.cs
+++ ASP.NET-CORE-Web-App/ApiaryDiary.Data/Models/QueenBee.cs
@@ -1,6 +1,17 @@
 ﻿namespace ApiaryDiary.Data.Models
 {
+    using ApiaryDiary.Data.Models.Enums;
+
     public class QueenBee
     {
+        public int Id { get; set; }
+
+        public QueenBeeType Type { get; set; }
+
+        public string MarkingColour { get; set; }
+
+        public string Origin { get; set; }
+
+        public string Temper { get; set; }
     }
 }

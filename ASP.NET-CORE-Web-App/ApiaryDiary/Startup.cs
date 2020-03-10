c132629a0ca5adb7e2dac20aadb63b683a27d820
DeniPanov <Inedvonap@gmail.com>
2020-03-10 07:58:10 +0200

    Add password options and automate migrations in the Startup.cs

--- ASP.NET-CORE-Web-App/ApiaryDiary/Startup.cs
+++ ASP.NET-CORE-Web-App/ApiaryDiary/Startup.cs
@@ -5,9 +5,11 @@ namespace ApiaryDiary
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Hosting;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using ApiaryDiary.Data.Models;
 
     public class Startup
     {
@@ -27,12 +29,34 @@ namespace ApiaryDiary
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApiaryDiaryDbContext>();
 
+            services.Configure<IdentityOptions>(options => //See if there is a better way to do this.
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 0;
+            });
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApiaryDiaryDbContext>();
+
+                if (env.IsDevelopment())
+                {
+                    dbContext.Database.Migrate();
+                }
+
+                //new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
+            }
+
             app.UseExceptionHandling(env);
 
             app.UseHttpsRedirection();

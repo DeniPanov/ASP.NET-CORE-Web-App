b2232f806437409ae9d1260e7c5c85bfeef5101f
DeniPanov <Inedvonap@gmail.com>
2020-03-18 21:39:56 +0200

    Add dataAnotations migration

---
+++ ASP.NET-CORE-Web-App/ApiaryDiary.Data/Migrations/20200318080827_AddedDataAnotations.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiaryDiary.Data.Migrations
+{
+    public partial class AddedDataAnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Locations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Temper",
+                table: "QueenBees",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Origin",
+                table: "QueenBees",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MarkingColour",
+                table: "QueenBees",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Notes",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Settlement",
+                table: "Locations",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Settlement",
+                table: "Locations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Temper",
+                table: "QueenBees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Origin",
+                table: "QueenBees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MarkingColour",
+                table: "QueenBees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Notes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}

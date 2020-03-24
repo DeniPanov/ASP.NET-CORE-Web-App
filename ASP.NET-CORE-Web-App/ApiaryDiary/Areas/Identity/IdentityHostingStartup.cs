8c4e632fe3cc094fc1405cd22a9091544e96e8dc
DeniPanov <Inedvonap@gmail.com>
2020-03-24 12:53:32 +0200

    Add logo in the navbar, add Views/Apiaries/Create.cshtml

---
+++ ASP.NET-CORE-Web-App/ApiaryDiary/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ApiaryDiary.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ApiaryDiary.Areas.Identity.IdentityHostingStartup))]
+namespace ApiaryDiary.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

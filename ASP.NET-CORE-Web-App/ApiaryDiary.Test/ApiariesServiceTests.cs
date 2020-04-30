f8aa65a1bd58656dc5d8e2240cf3486b9ebdb68c
DeniPanov <Inedvonap@gmail.com>
2020-04-30 14:28:14 +0300

    Add some tests

--- ASP.NET-CORE-Web-App/ApiaryDiary.Test/ApiariesServiceTests.cs
+++ ASP.NET-CORE-Web-App/ApiaryDiary.Test/ApiariesServiceTests.cs
@@ -10,36 +10,47 @@
     using Xunit;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.AspNetCore.Mvc.Testing;
+    using ApiaryDiary.Data.Models;
+    using System;
+    using ApiaryDiary.Data.Models.Enums;
 
     public class ApiariesServiceTests
     {
         public ApiariesServiceTests()
         {
+            
         }
 
-        [Fact]
         //[Theory]
         //[InlineData("")]
         //[InlineData("")]
-        public void ApiariesCountIsValid()
-        {
-            var mockService = new Mock<IApiaryService>();
-            // mockService.Setup(x => x.CreateAsync(...)).Returns(...);
+        //[Fact]
+        //public void ApiariesCountIsValid()
+        //{
+        //    var mockService = new Mock<IApiaryService>();
+        //    mockService.Setup(x => x.ApiariesCount()).Returns(1);
 
-            // mockService.Verify(x => x.CreateAsync(), Times.AtLeastOnce);
-        }
+        //    mockService.Verify(x => x.ApiariesCount(), Times.AtLeastOnce);
+        //}
 
         [Fact]
-        public void InMemoryDbTest()
+        public async Task ApiariesCountIsValid()
         {
             var options = new DbContextOptionsBuilder<ApiaryDiaryDbContext>()
                 .UseInMemoryDatabase("DbTest").Options;
-
             using var db = new ApiaryDiaryDbContext(options);
+
             var locationService = new LocationInfoService(db);
             var apiaryService = new ApiaryService(db, locationService);
 
+            var userId = "dasadadadadas";
+            var apiaryName = "Some appropriate name";
+            var capacity = 100;
 
+            var apiaryId = 
+                await apiaryService.CreateAsync(userId, apiaryName, capacity);
+
+            Assert.Equal(1, apiaryService.ApiariesCount());
         }
 
         [Fact] // Integration Test
@@ -51,7 +62,7 @@
             var response = await client.GetAsync("/Apiaries/ViewAll");
             var responseAsString = await response.Content.ReadAsStringAsync();
 
-            Assert.Contains("TotalBeehives", responseAsString);
+            Assert.Contains("__RequestVerificationToken", responseAsString);
         }
     }
 }
